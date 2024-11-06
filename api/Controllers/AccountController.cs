7038f9932c7e5223d4d0a2ef2882a2672a6804dd
zhiyizhu <zhiyi.zhu@hotmail.com>
2024-11-07 00:03:58 +1300

    Configure and generate JWT with token descriptor setup and signing credentials

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Dtos.Account;
+using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -14,9 +15,12 @@ namespace api.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
-        public AccountController(UserManager<AppUser> userManager)
+        private ITokenService _tokenService;
+        public AccountController(UserManager<AppUser> userManager,ITokenService tokenService)
         {
             _userManager = userManager;
+            _tokenService = tokenService;
+
         }
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
@@ -39,7 +43,14 @@ namespace api.Controllers
                     //UserManager<AppUser> is a service provided by ASP.NET Core Identity, injected via dependency injection（through the AddIdentity method in builder.Services.）
                     if (roleResult.Succeeded)
                     {
-                        return Ok("User created");
+                        return Ok(
+
+                            new NewUserDto{
+                                UserName = appUser.UserName,
+                                Email = appUser.Email,
+                                Token = _tokenService.CreateToken(appUser), // creates and returns a new token to the client for immediate use without needing to save it in a database.
+                            }
+                        );
                     }
                     else
                     {
--- api/Program.cs
+++ api/Program.cs
@@ -2,6 +2,7 @@ using api.Data;
 using api.Interfaces;
 using api.Models;
 using api.Repository;
+using api.Service;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -61,6 +62,7 @@ System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
 
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
